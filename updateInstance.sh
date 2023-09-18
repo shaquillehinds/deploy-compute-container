40890c7289f688670d6033826fb2faee7e2c4293
Shaquille <shaqdulove@hotmail.com>
2023-09-18 14:03:15 -0400

    Added the ability to define the machine_type

--- updateInstance.sh
+++ updateInstance.sh
@@ -5,6 +5,12 @@ if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
   exit 1
 fi
 
+if [ -z $4 ]; then
+  machine_type=e2-micro
+else
+  machine_type=$4
+fi
+
 instancesList=$(gcloud compute instances list --filter "name=('$1')")
 
 echo $instancesList
@@ -12,14 +18,14 @@ echo $instancesList
 if [ -z "${instancesList}" ]; then
 
   echo "No instance found"
-  echo "Running: gcloud compute instances create-with-container $1 --zone=$2 --container-image=$3 --machine-type=e2-micro --tags=http-server,https-server --container-env=$4"
+  echo "Running: gcloud compute instances create-with-container $1 --zone=$2 --container-image=$3 --machine-type=$machine_type --tags=http-server,https-server --container-env=$5"
 
-  gcloud compute instances create-with-container $1 --zone=$2 --container-image=$3 --machine-type=e2-micro --tags=http-server,https-server --container-env=$4
+  gcloud compute instances create-with-container $1 --zone=$2 --container-image=$3 --machine-type=$machine_type --tags=http-server,https-server --container-env=$5
 
 else 
 
   echo "Instance found"
-  ecno "Running: gcloud compute instances update-container $1 --zone=$2 --container-image=$3 --container-env=$4"
+  ecno "Running: gcloud compute instances update-container $1 --zone=$2 --container-image=$3 --container-env=$5"
 
-  gcloud compute instances update-container $1 --zone=$2 --container-image=$3 --container-env=$4
+  gcloud compute instances update-container $1 --zone=$2 --container-image=$3 --container-env=$5
 fi
